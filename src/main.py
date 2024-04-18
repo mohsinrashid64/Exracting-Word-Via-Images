f836e633327e23acde9b57b5627b3b799be7e10d
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2024-04-18 12:20:37 +0500

    Best-Practices-1st-commit

--- src/main.py
+++ src/main.py
@@ -3,26 +3,22 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import config
 from fastapi import FastAPI
-from utils import encode_image
+from utils import encode_image, extract_file_type
 import requests
 
 
-
 app = FastAPI()
-
-
 
 @app.get("/")
 def read_root():
     return {'response': "API RUNNING"}
 
 
-@app.get("/extract_text_from_image")
-def extract_text_from_image(image_path: str):
-    try:
-        base64_image = encode_image(image_path)
-    except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="The file path provided does not exist.")
+@app.get("/extract_text_from_image/")
+def extract_text_from_image(file_path: str):
+
+    base64_image = encode_image(file_path)
+
     headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {config.OPENAI_API_KEY}"
@@ -35,14 +31,14 @@ def extract_text_from_image(image_path: str):
             "content": [{
                 "type": "image_url",
                 "image_url": {
-                    "url": f"data:image/jpg;base64,{base64_image}"
+                    "url": f"data:image/" + extract_file_type(file_path) + f";base64,{base64_image}"
                 }
             }]
         }],
             'max_tokens':1000,
         }
         response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-        print(response.json())
+        # print(f"data:image/"+ extract_file_type(file_path) + f";base64,{base64_image}")
         return response.json()
     
     except Exception as e:
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,17 @@
+import os
 import base64
+from fastapi import HTTPException
 
-def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+def encode_image(file_path):
+  try:
+    with open(file_path, "rb") as image_file:
+      return base64.b64encode(image_file.read()).decode('utf-8')
+  
+  except FileNotFoundError:
+      raise HTTPException(status_code=404, detail="The file path provided does not exist.")
+
+
+def extract_file_type(file_path):
+    _, file_extension_with_dot = os.path.splitext(file_path)
+    return file_extension_with_dot[1:]
+
