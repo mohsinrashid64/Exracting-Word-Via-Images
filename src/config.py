df9d9bf3fa3de831f682c4aeda146762f0b7ee61
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2024-04-20 14:33:08 +0500

    For-Deployment-4th-commit

--- src/config.py
+++ src/config.py
@@ -7,7 +7,39 @@ PROMPT =  [
         "role": "user",
         "content": [{
                         "type": "text", 
-                        "text": "What: Please help me extract the textual pieces of information from the image provided.\n\nWho: I am a professional Mobile application developer in Flutter.\n\nWhy: This question originates from a company that makes mobile applications. They are making an e-commerce app and they intend to add a feature in their app that allows the user to upload images and use generative AI to extract the relevant data from the image.\n\nWhich: The data you are supposed to extract from the image is 'Balance Date'(Date should be in this format: 'DD/MM/YYYY'), 'Cash On Hand(this could have different name for instance 'Cash', 'Cash Available' or any other synanoms or phrases with the same meaning, Please do not confuse this with Bank Balance)', 'Bank Name',’Bank Balance’, 'Product List'(this could be more than one and have the following attributes, also correct the spelling mistake if you are familiar with the product otherwise do not do it): “Product Name”, “Quantity”, “Delivery Charges” and “Cost”. The wording of the aforementioned fields may differ so extract according to the meaning.Please ignore anything else. If you are unable to find any field for those put “N/A”. Furthermore the this is the format you are supposed to return the content:\n\n\n{\n\t\"last_balance_sheet_date\" : \"<RELEVANT_DATA>\",\n\t\"cash_on_hand\":\"<RELEVANT_DATA>\",\n\t\"bank_name\":\"<RELEVANT_DATA>\",\n\t\"bank_balance\":\"<RELEVANT_DATA>\",\n\t\"list_of_products\":[\n\t\t{\n\t\t\t\"product_name\":\"<RELEVANT_DATA>\",\n\t\t\t\"product_cost\":\"<RELEVANT_DATA>\",\n\t\t\t\"quantity\":\"<RELEVANT_DATA>\",\n\t\t\t\"delivery_charges\":\"<RELEVANT_DATA>\"\n\t\t}\n\t]\nHow: Finally, return the data as a JSON response and only JSON nothing else."
+                        "text": '''
+                                What:
+                                Please help me Extract the textual pieces of information from the image provided.
+                                Who:
+                                I am a professional Mobile application developer in flutter. Please answer my queries as an experienced mobile developer and as a web scraper and someone who is an expert in image processing and generative AI.
+
+                                Why:
+                                This question originates from a company that makes mobile applications and they are making an ecommerce app and they are intending  to add a feature in their app that allows the user to upload images and use generative AI to extract the relevant data from the image. The image could be a screenshot of doc or pdf, in short it contains text. The image could also contain handwritten text.
+
+                                Which:
+                                The data you are supposed to extract from the image is “Balance Date”, “Cash”, “Bank”, “Product List”(this could be more than one and have the following attributes): “Product Name”, Unit(this is the weight and could be either in kilograms or pounds, also convert them to the approperiate abbreviation), “Quantity”, “Delivery Charges” and “Cost”. The wording of the aforementioned fields may differ so extract according to the meaning.Please ignore anything else. If you are unable to find any field for those put “N/A”. Furthermore the this is the format you are supposed to return the content:
+                                {
+                                    "last_balance_sheet_date" : "<RELEVANT_DATA>",
+                                    "cash_on_hand":"<RELEVANT_DATA>",
+                                    "bank_name":"<RELEVANT_DATA>",
+                                    "bank_balance":"<RELEVANT_DATA>",
+                                    "list_of_products":[
+                                        {        
+                                            "product_name":"<RELEVANT_DATA>",
+                                            "unit": "<RELEVANT_DATA>",
+                                            "product_cost":"<RELEVANT_DATA>",
+                                            "quantity":"<RELEVANT_DATA>",
+                                            "delivery_charges":"<RELEVANT_DATA>"
+                                        }
+                                    ]
+                                }
+
+
+
+                                How:
+                                Finally return the data as a JSON response and only a json response nothing else this is of the utmost importance.
+
+                                '''
                     }
         ]
     }
