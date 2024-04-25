9995d4da1e04458c58f19f0ff83f7eea8dbc314f
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2024-04-25 18:29:05 +0500

    For-Deployment-6th-commit

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-product_list =  [
+product_purchase =  [
     {
         "role": "system",
         "content": "You are an experienced mobile developer, a web scraper, and an expert in image processing and generative AI. You will be provided with an image, which could be a screenshot of a document or PDF, or it could contain handwritten text. Your task is to extract all of the relevant data from the image and return it in a specified format."
@@ -8,37 +8,63 @@ product_list =  [
         "content": [{
                         "type": "text", 
                         "text": '''
-                                What:
-                                Please help me Extract the textual pieces of information from the image provided.
-                                Who:
-                                I am a professional Mobile application developer in flutter. Please answer my queries as an experienced mobile developer and as a web scraper and someone who is an expert in image processing and generative AI.
+                                Please help me extract the textual pieces of information from the image provided. Your job is to extract all of the aforementioned from the image.The desired output I want to receive is in JSON format and just this nothing else. If you are unable to find any of the details in the image please add ‘N/A’ in the json with the respective key. Please observe the image carefully and extract the words as they are in the image, regardless whether they make sense or not.
 
-                                Why:
-                                This question originates from a company that makes mobile applications and they are making an ecommerce app and they are intending  to add a feature in their app that allows the user to upload images and use generative AI to extract the relevant data from the image. The image could be a screenshot of doc or pdf, in short it contains text. The image could also contain handwritten text.
+                                I am a professional Mobile application developer in Flutter. I am making an application for my client. The app is an e-commerce app and they intend to add a feature in their app that allows the user to upload images and use generative AI to extract the relevant data from the image. This data will be used to fill in the input fields.
 
-                                Which:
-                                The data you are supposed to extract from the image is “Balance Date”, “Cash”, “Bank”, “Product List”(this could be more than one and have the following attributes): “Product Name”, Unit(this is the weight and could be either in kilograms or pounds, also convert them to the approperiate abbreviation), “Quantity”, “Delivery Charges” and “Cost”. The wording of the aforementioned fields may differ so extract according to the meaning.Please ignore anything else. If you are unable to find any field for those put “N/A”. Furthermore the this is the format you are supposed to return the content:
+                                This app is intended for businesses that wish to maintain and store their records efficiently, enabling them to store data simply by capturing a photo of their documents.
+
+                                The data that I want you extract from the image are the following:
+                                1. Delivery Cost
+                                2. Payment Mode 
+                                3. Vendor (this could have the following details):
+                                    1. Name
+                                    2. Street Address
+                                    3. City 
+                                    4. State
+                                    5. Zipcode
+                                    6. Phone Number
+                                    7. Email Address
+                                4. Purchase Date
+                                5. Reference Number
+                                6. Purchase Description
+                                7. Products List (this could have the following details):
+                                    1. Product Name
+                                    2. Amount 
+                                    3. Product Quantity
+
+                                Please note that, all of the aforementioned fields may not have the same wording as given, rather you should extract the relevant data according to meaning and context.
+
+                                Finally I want the response to be in a JSON format and just that, nothing else, do not even add ```json or anything else like punctuations or any other thing, this is of the utmost importance when it comes to the response. The JSON response needs to have the following structure:
                                 {
-                                    "last_balance_sheet_date" : "<RELEVANT_DATA>",
-                                    "cash_on_hand":"<RELEVANT_DATA>",
-                                    "bank_name":"<RELEVANT_DATA>",
-                                    "bank_balance":"<RELEVANT_DATA>",
-                                    "list_of_products":[
-                                        {        
-                                            "product_name":"<RELEVANT_DATA>",
-                                            "unit": "<RELEVANT_DATA>",
-                                            "product_cost":"<RELEVANT_DATA>",
-                                            "quantity":"<RELEVANT_DATA>",
-                                            "delivery_charges":"<RELEVANT_DATA>"
-                                        }
+                                    'delivery_cost': '<value>',
+                                    'payment_mode': '<value>',
+                                    'vendor': [
+                                        {
+                                            'name': '<value>',
+                                            'street_address': '<value>',
+                                            'city': '<value>', 
+                                            'state': '<value>',
+                                            'zipcode': '<value>',
+                                            'phone_number': '<value>',
+                                            'email_address': '<value'>
+                                        }.
+                                …
+                                    ],
+                                    'purchase_date': '<value>',
+                                    'reference_number':  '<value>',
+                                    'purchase_description': '<value>',
+                                    'products_list': [
+                                        {
+                                            'product_name': '<value>,'
+                                            'amount': '<value>,'
+                                            'product_quantity': '<value>'
+                                        },
+                                        …
                                     ]
                                 }
 
-
-
-                                How:
-                                Finally return the data as a JSON response and only a json response nothing else this is of the utmost importance. Do not put anything else in the response just a JSON please. Do not put "```json" this in the response.
-
+                                Replace the “<value>” with the appropriate data you extract from the image. The “...” in the JSON structure denotes that there could be multiple entries.
                                 '''
                     }
         ]
@@ -47,76 +73,3 @@ product_list =  [
 
 
 
-
-invoice_list =  [
-    {
-        "role": "system",
-        "content": "You are an experienced mobile developer, a web scraper, and an expert in image processing and generative AI. You will be provided with an image, which could be a screenshot of a document or PDF, or it could contain handwritten text. Your task is to extract all of the relevant data from the image and return it in a specified format."
-    },
-    {
-        "role": "user",
-        "content": [{
-                        "type": "text", 
-                        "text": '''
-                               What: 
-                                Please help me extract the textual pieces of information from the image provided. The image you will receive is that of and invoice, it can and will have the following as its content:
-                                Title of Invoice (this could be ‘Cash Sale’, ‘Product Purchase’ or ‘Invoice’)
-                                Date (Convert it to “DD/MM/YYYY” format) 	
-                                Reference Number (Ref No)	
-                                Customer Details (this will contain the following details. I.e, name, address, phone number, email, credit terms)
-                                Business Details (this will contain the following details. I.e, name, address, phone number, email)
-                                Data in tabular format which will have the following:
-                                Item
-                                Price(USD)
-                                Quantity or Qty
-                                Add Tax
-                                Total (USD)
-
-                                Your job is to extract all of the aforementioned from the image.The desired output I want to receive is in JSON format and just this nothing else. If you are unable to find any of the aforementioned details in the image please add ‘N/A’ in the json with the respective key.
-
-                                Who:
-                                I am a professional Mobile application developer in Flutter.
-
-                                Why:
-                                This question originates from a company that makes mobile applications. They are making an e-commerce app and they intend to add a feature in their app that allows the user to upload images and use generative AI to extract the relevant data from the image.
-                                Which:
-                                You have to extract everything specified as before.As for the tabular part please observe the column names of the table as they are supposed to be key names in the JSON also populate each key with data provided with respect to each column.The data you are supposed to extract from the image is 'Item', 'Price(USD), 'Quantity', 'Add Tax', 'Total (USD)', Further more in the table there is also a part which contains sections named ‘Sub Total’, ‘Tax’, ‘Total’, please also retrieve these. Please note that the wording of the column names may differ, please  extract them according to the context.Following is the template on how you should create the JSON:
-                                {
-                                "title_of_invoice": "<value>",
-                                "date": "<value>",
-                                "ref_no": "<value>",
-                                "customer_details": {
-                                    "name": "<value>",
-                                    "address": "<value>",
-                                    "phone_number": "<value>",
-                                    "email": "<value>",
-                                    "credit_terms": "<value>"
-                                },
-                                "business_details": {
-                                    "name": "<value>",
-                                    "address": "<value>",
-                                    "phone_number": "<value>",
-                                    "email": "<value>"
-                                },
-                                "item_list": [
-                                    {
-                                    "item": "<value>",
-                                    "price_usd": "<value>",
-                                    "quantity": "<value>",
-                                    "add_tax": "<value>",
-                                    "total_usd": "<value>"
-                                    }
-                                ],
-                                "total_prices": {
-                                    "sub_total": "<value>",
-                                    "tax": "<value>",
-                                    "total": "<value>"
-                                }
-                                }
-                                How: 
-                                Finally return the data as a JSON response and only a json response nothing else this is of the utmost importance. Do not put "```json" this in the response.
-                                '''
-                    }
-        ]
-    }
-]
